385c343e08b9f244f1bff9bae15924096ad14260
Mathis Micheau <mathis.micheau@etu.unilim.fr>
2024-02-05 15:56:14 +0100

    V05/02

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -2,6 +2,7 @@
 
 
 /* indique où "vit" ce fichier */
+
 namespace App\Controller;
 
 
@@ -14,11 +15,12 @@ use stdClass;
 use App\Entity\Lego;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
+
 class LegoController extends AbstractController
 {
-   // L’attribute #[Route] indique ici que l'on associe la route
-   // "/" à la méthode home pour que Symfony l'exécute chaque fois
-   // que l'on accède à la racine de notre site.
+    // L’attribute #[Route] indique ici que l'on associe la route
+    // "/" à la méthode home pour que Symfony l'exécute chaque fois
+    // que l'on accède à la racine de notre site.
 
     private array $legos;
 
@@ -27,7 +29,7 @@ class LegoController extends AbstractController
         $this->legos = [];
         $data = file_get_contents("../src/data.json");
         $tab = json_decode($data);
-        
+
 
         foreach ($tab as $legoData) {
 
@@ -40,25 +42,57 @@ class LegoController extends AbstractController
             $legoModel->setlegoImage($legoData->images->bg);
 
             array_push($this->legos, $legoModel);
-     
         }
-        
+    }
+    #[Route('/me',)]
+    public function me()
+    {
+        die("Mathis");
     }
 
-   #[Route('/', )]
-   public function home()
-   {
 
-    return $this->render('lego.html.twig', ['legos' => $this->legos]);
-   }
+    #[Route('/',)]
+    public function home()
+    {
 
-   #[Route('/me', )]
-   public function me()
-   {
-       die("Mathis");
-   }
+        return $this->render('lego.html.twig', ['legos' => $this->legos]);
+    }
+
+
+
+
+    //    #[Route('/creator', )]
+    //    public function creator()
+    //    {
+
+    //     return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator" ; }) ]);
+    //    }
+
+    //    #[Route('/star_wars', )]
+    //    public function starwars()
+    //    {
+
+    //     return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Star Wars" ; }) ]);
+    //    }
+
+
+    //    #[Route('/creator_expert', )]
+    //    public function creatorExpert()
+    //    {
+
+    //     return $this->render('lego.html.twig', ['legos' => array_filter($this->legos, function($lego) { return $lego->getCollection() === "Creator Expert" ; }) ]);
+    //    }
+
+    #[Route('/{collection}', name: 'filter_by_collection', requirements: ['collection' => '(creator|star_wars|creator_expert)'])]
+    public function filter($collection): Response
+    {
+
+        
+        return $this->render('lego.html.twig', [
+            'legos' => array_filter($this->legos, function ($legoData) use ($collection) {
+                
+                return strtolower($legoData->getCollection()) == str_replace('_', ' ', strtolower($collection));
+            })
+        ]);
+    }
 }
-
-
-
-?>
